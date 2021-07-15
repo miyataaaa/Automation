e210f8bd45c310ce8d81e985f102b394dc1e537e
Rentarou Miyata <miyataaaa1203@gmail.com>
2021-07-15 17:33:52 +0900

    add new macro for graph view

--- automatic_chiplot.py
+++ automatic_chiplot.py
@@ -1,7 +1,10 @@
 import xlwings as xw
 import os
-path_1 = r"C:\Users\miyar\OneDrive\デスクトップ\四年\VBA\automation"
-path_2 = r"C:\Users\miyar\OneDrive\デスクトップ\四年\VBA\chiplot\still"
+
+#保存先のディレクトリ
+path_1 = r"C:\Users\miyar\OneDrive\デスクトップ\四年\VBA\automation\m075_n195"
+#元ファイルがあるディレクトリ
+path_2 = r"C:\Users\miyar\OneDrive\デスクトップ\四年\VBA\automation\none_nahari"
 files = os.listdir(path_2)
 
 for filename in files:
@@ -23,20 +26,25 @@ for filename in files:
         wb_2.sheets[delsht_name[i]].delete()
 
 
-    input_nm = {"m_setting" : [0.8, 1, 0.05],
-                "n_setting" : [1.8, 2, 0.05]
+    input_nm = {"m_setting" : [0.7, 0.8, 0.05],
+                "n_setting" : [1.85, 1.95, 0.05]
                 }
 
     sht2 = wb_2.sheets['Sheet2']
     sht2.range("B3").value = list(input_nm.values())[0]
     sht2.range("B4").value = list(input_nm.values())[1]
-
+    
+    # χプロットを作成するマクロ
     macro_chi = wb_2.macro("ChiMulti2")
     macro_chi()
 
+    # すべての河川、m・n組み合わせでの決定係数を一覧表示するためのマクロ
     macro_R2 = wb_2.macro("Calc_R2")
     macro_R2()
 
+    # すべての χプロットを1つのシート内に配置するためのマクロ
+    macro_graph = wb_2.macro("chartCP")
+    macro_graph()
     wb_2.save()
     wb_2.close()
 
